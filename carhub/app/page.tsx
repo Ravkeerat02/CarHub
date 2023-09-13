b19ac4ae8fafd930a12e54970972f729f7b31609
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-09-13 16:00:39 -0400

    complete

--- carhub/app/page.tsx
+++ carhub/app/page.tsx
@@ -36,6 +36,7 @@ export default async function Home({ searchParams }: HomeProps) {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
+                // eslint-disable-next-line react/jsx-key
                 <CarCard car={car} />
               ))}
             </div>
