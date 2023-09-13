9b63e7f41e96341ba175740d0d5bf823ba2a2971
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-09-13 03:22:14 -0400

    Detail part complete(img not displayed)

--- carhub/utils/index.ts
+++ carhub/utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "0a50b360c0msh895e3a524e1e0cap173a53jsnc221c6b475d8",
@@ -5,7 +7,7 @@ export async function fetchCars() {
   };
   //    will be used to dispaly all the cars
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=ferrari",
     {
       headers: headers,
     }
@@ -27,4 +29,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, year, model } = car;
+  url.searchParams.append("customer", "hrjavascript-mastery");
+
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
